96a5cc99bb6f2f6ee1b126eb3801bc373d671a37
Tim Anema <tim.anema@shopify.com>
2020-05-19 18:50:34 -0400

    Updates

---
+++ precache-manifest.dc2efabc52fbc92cb63b0ba287a059d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2888157b91b564e6fa127d49b37b8b84",
+    "url": "/tubes-chat/index.html"
+  },
+  {
+    "revision": "cb87ea6437331cce3ff6",
+    "url": "/tubes-chat/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "cb87ea6437331cce3ff6",
+    "url": "/tubes-chat/static/js/2.d4baf8a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tubes-chat/static/js/2.d4baf8a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6b043e3a006c3b769c",
+    "url": "/tubes-chat/static/js/main.81a1fa3a.chunk.js"
+  },
+  {
+    "revision": "4dc5875076c105f0f56e",
+    "url": "/tubes-chat/static/js/runtime-main.8f5ffa9d.js"
+  }
+]);
