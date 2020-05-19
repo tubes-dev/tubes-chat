62b8ded11967753bc7a56cf8cc17b300819953ee
Tim Anema <tim.anema@shopify.com>
2020-05-19 17:35:21 -0400

    moved build to docs

---
+++ docs/precache-manifest.9d3d311d5d748eee2f65d55429c86ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6c7c1d58992125a89406c61e91dac8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb87ea6437331cce3ff6",
+    "url": "/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "cb87ea6437331cce3ff6",
+    "url": "/static/js/2.d4baf8a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4baf8a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6b043e3a006c3b769c",
+    "url": "/static/js/main.81a1fa3a.chunk.js"
+  },
+  {
+    "revision": "7bfcb63af6bbc56de29b",
+    "url": "/static/js/runtime-main.e8a754d5.js"
+  }
+]);
